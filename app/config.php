fa6ea070c223f69eea908b4a486193c835b602a6
Vinicius <vickosantos@gmail.com>
2017-06-27 16:09:16 -0300

    Configuração do hxphp framework

--- app/config.php
+++ app/config.php
@@ -4,6 +4,17 @@ $configs = new HXPHP\System\Configs\Config;
 
 $configs->env->add('development');
 
+$configs->env->development->baseURI = '/sistema/';
+
+$configs->env->development->database->setConnectionData(
+  array(
+  'host' => 'localhost',
+  'user' => 'root', 
+  'password' => '',
+  'dbname' => 'sistemahx'
+  )
+);
+
 /*
   //Globais
   $configs->title = 'Titulo customizado';
