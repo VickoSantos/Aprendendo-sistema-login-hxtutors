0e3c498db1b0f0003e897409c3b2fb604484481a
Vinicius <vickosantos@gmail.com>
2017-06-29 00:47:37 -0300

    Add required

--- app/controllers/CadastroController.php
+++ app/controllers/CadastroController.php
@@ -2,5 +2,14 @@
 
 class CadastroController extends \HXPHP\System\Controller
 {
-	
+	public function cadastrarAction()
+	{
+		$this->view->setFile('index');
+
+		$this->request->setCustomFilters(array(
+			'email' => FILTER_VALIDATE_EMAIL
+		));
+
+		$cadastrarUsuario = User::cadastrar($this->request->post());
+	}
 }
